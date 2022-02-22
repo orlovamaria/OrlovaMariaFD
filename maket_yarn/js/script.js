76089cd02154bfbaf6ec380a0b9ed7193f1b9d40
orlovamaria <88633050+orlovamaria@users.noreply.github.com>
2022-02-22 18:05:06 +0300

    maket

--- maket_yarn/js/script.js
+++ maket_yarn/js/script.js
@@ -1,3 +1,5 @@
+//slider
+
 $(document).ready(function(){
     $('.slider').slick({
         slidesToShow: 5,
@@ -5,3 +7,80 @@ $(document).ready(function(){
         waitForAnimate: false,
     });
 });
+
+
+//Стрелки формы
+$(document).ready(function(){
+    $('.block-title').click(function(event) {
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+});
+
+// Анимация объектов страниц
+
+const animitems = document.querySelectorAll('._anim-items');
+
+if (animitems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(params) {
+        for (let index = 0; index < animitems.length; index++) {
+            const animitem = animitems[index];
+            const animitemHeight = animitem.offsetHeight;
+            const animitemOffset = offset(animitem).top;
+            const animStart = 4;
+
+            let animitemPoint = window.innerHeight - animitemHeight / animStart;
+
+            if (animitemHeight > window.innerHeight) {
+                animitemPoint = window.innerHeight - animitemHeight / animStart;
+            }
+
+            if ((pageYOffset > animitemOffset - animitemPoint) && pageYOffset < (animitemOffset + animitemHeight)) {
+                animitem.classList.add('_active');
+            } else {
+                if(!animitem.classList.contains('_anim-no-hide')) {
+                    animitem.classList.remove('_active');
+                }
+                animitem.classList.remove('_active');
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+           scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+           scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout (() => {
+        animOnScroll();
+    }, 300);
+}
+
+
+//Выпадающее меню
+
+//let isMobile = {
+	//Android: function() {return navigator.userAgent.match(/Android/i);},
+	//BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	//iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	//Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	//Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	//any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+//};
+		//let body=document.querySelector('body');
+//if(isMobile.any()){
+		//body.classList.add('touch');
+		//let arrow=document.querySelectorAll('.arrow');
+	//for(i=0; i<arrow.length; i++){
+			//let thisLink=arrow[i].previousElementSibling;
+			//let subMenu=arrow[i].nextElementSibling;
+			//let thisArrow=arrow[i];
+
+			//thisLink.classList.add('parent');
+		//arrow[i].addEventListener('click', function(){
+			//subMenu.classList.toggle('open');
+			//thisArrow.classList.toggle('active');
+		//});
+	//}
+//}else{
+	//body.classList.add('mouse');
